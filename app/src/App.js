eff888f1d5fd967e5cabdc4055c469f073c2b212
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-12-16 12:00:42 -0500

    Worked some more on Form.js, as well as added some state.

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+
+// Components
+import Form from "./components/Form/Form";
+import List from "./components/List/List";
+
+import "./App.css";
+
+const initialFormValues = {
+	name: "",
+	email: "",
+	password: "",
+	termsOfService: false,
+};
+
+const initialUsersValues = [];
+const initialDisabled = true;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState(initialUsersValues);
+	const [formValues, setFormValues] = useState(initialFormValues); // Object
+	const [disabled, setDisabled] = useState(initialDisabled); // Boolean
+
+	// const [listOfUsers, setListOfUsers] = useState();
+
+	const change = (name, value) => {
+		setFormValues({
+			...formValues,
+			[name]: value,
+		});
+	};
+
+	useEffect(() => {
+		// Axios Request here
+		axios
+			.get("https://reqres.in/api/users")
+			.then((res) => console.log(res))
+			.catch((err) => console.error(err));
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Users App</h1>
+			<Form data={formValues} change={change} disabled={disabled} />
+			<List />
+		</div>
+	);
 }
 
 export default App;
